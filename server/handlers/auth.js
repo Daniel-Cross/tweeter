434680dd0547bcfc6a635138cab5dcbd081ef1e7
Daniel Cross <crossy1686@gmail.com>
2019-04-06 18:55:32 +0200

    signup handler added that compares passwords

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,33 +1,67 @@
-const db = require("../models");
-const jwt = require("jsonwebtoken");
+const db = require('../models');
+const jwt = require('jsonwebtoken');
 
-exports.signin = async function(req, res, next) {};
+exports.signin = async function(req, res, next) {
+	try {
+		let user = await db.user.findOne({
+			email: req.body.email,
+		});
+		let { id, username, profileImageUrl } = user;
+		let isMatch = await user.comparePassword(req.body.password);
+		if (isMatch) {
+			let token = jwt.sign(
+				{
+					id,
+					username,
+					profileImageUrl,
+				},
+				process.env.SECRET_KEY
+			);
+			return res.status(200).json({
+				id,
+				username,
+				profileImageUrl,
+				token,
+			});
+		} else {
+			return next({
+				status: 400,
+				message: 'Invalid or email/password combination',
+			});
+		}
+	} catch (err) {
+		return next({
+			status: 400,
+			message: 'Invalid or email/password combination',
+		});
+	}
+};
 
 exports.signup = async function(req, res, next) {
-  try {
-    let user = await db.User.create(req.body);
-    let { id, username, profileImageUrl } = user;
-    let token = jwt.sign(
-      {
-        id,
-        username,
-        profileImageUrl
-      },
-      process.env.SECRET_KEY
-    );
-    return res.status(200).json({
-      id,
-      username,
-      profileImageUrl,
-      token
-    });
-  } catch (err) {
-    if (err.code === 11000) {
-      err.message = "Sorry, that username and/or email is taken";
-    }
-    return next({
-      status: 400,
-      message: err.message
-    });
-  }
+	try {
+		let user = await db.User.create(req.body);
+		let { id, username, profileImageUrl } = user;
+		let token = jwt.sign(
+			{
+				id,
+				username,
+				profileImageUrl,
+			},
+			process.env.SECRET_KEY
+		);
+		return res.status(200).json({
+			id,
+			username,
+			profileImageUrl,
+			token,
+		});
+	} catch (err) {
+		if (err.code === 11000) {
+			err.message = 'Sorry, that username and/or email is taken';
+		}
+		return next({
+			status: 400,
+			message: err.message,
+		});
+	}
 };
